4f91cd4848871838b3f10753e9ae52a808eae3bf
lary <f98f96@gmail.com>
2020-12-01 14:03:57 +0300

    Initial commit

---
+++ VKTalker/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace VKTalker.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
